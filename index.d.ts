23c1cc37a3944034b00b606ea43a7e7fdac91ff9
Tim Mensch <tim-sg-chimera@bitgems.com>
2018-07-14 17:13:52 -0600

    Change core API to be much simpler.

--- index.d.ts
+++ index.d.ts
@@ -1,55 +1,62 @@
 export enum SCEPluginTypes {
-  LANGUAGE = 'SCE_LANGUAGE'
+  LANGUAGE = 'SCE_LANGUAGE',
+}
+
+export interface SceLocation {
+  href: string;
 }
 
 export interface SceEvent {
-
+  eventName: string;
+  location: SceLocation;
 }
 
 export type SCECodeGenTypeContainer = {
-  SCEPlugin: SCECodeGenType
-}
-export type SCECodeGenType = new() => SceMain;
-
-export interface Updateable {
-  updateCode: () => any;
-}
+  SCEPlugin: SCECodeGenType;
+};
+export type SCECodeGenType = new () => SceMain;
 
 export interface SCEPluginElement {
-  url: string,
-  code: string,
-  dateCreated: string,
-  pluginName: string,
-  pluginType: string,
-  plugin: Object
+  url: string;
+  code: string;
+  dateCreated: string;
+  pluginName: string;
+  pluginType: string;
+  plugin: Object;
 }
 
+/**
+ * Main interface to the code generation plugin.
+ *
+ * @export
+ * @abstract
+ * @class SceMain
+ */
 export abstract class SceMain {
-  
   static pluginName: string;
   static pluginType: string;
-  
-  initialize: () => void;
-  
-  onComplete: (x: Updateable) => void;
-  
-  onNextEvent: (ev: SceEvent, x: Updateable) => void;
-  
-  getRawGeneratedCode: () => string;
-  
-  getStyledGeneratedCode: () => string;
-  
-}
 
-// declare enum Foods {
-//   CHERRY = 'CHERRY',
-//   LETTUCE = 'LETTUCE',
-//   JERKY = 'JERKY'
-// }
-//
-//
-// declare abstract class ForceFoods {
-//   static food : Foods
-// }
-//
-//
+  /**
+   * Set the sequence of events that we're generating code for.
+   *
+   * @abstract
+   * @memberof SceMain
+   */
+  abstract setEvents(ev: SceEvent[]): void;
+
+  /**
+   * Get RAW generated code.
+   *
+   * @memberof SceMain The generated code in no particular format.
+   */
+  abstract getRawGeneratedCode(): string;
+
+  /**
+   * Get formatted code.
+   *
+   * @abstract
+   * @returns {string} The generated code, styled and formatted.
+   * @memberof SceMain
+   */
+  abstract getStyledGeneratedCode(): string;
+}
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// Options for Prettier
+module.exports = {
+  // Let the lines be up to 90 characters wide
+  printWidth: 90,
+  // Use ugly single quotes everywhere, because that's what Alex likes.
+  singleQuote: true,
+  // Indent size: 2
+  tabWidth: 2,
+  // Indent with spaces
+  useTabs: false,
+  // Put trailing commas everywhere that ES5 allows
+  trailingComma: 'es5',
+};
